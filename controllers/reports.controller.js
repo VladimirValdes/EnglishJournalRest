2102425691323ea1ad779490ff8af650b905c778
Vladimir <mendozahum20@gmail.com>
2022-02-09 09:59:18 -0600

    feat: add generate reports for connectors, prepositions, adjectives and phrasalverbs

--- controllers/reports.controller.js
+++ controllers/reports.controller.js
@@ -5,7 +5,6 @@ const pdf = require('html-pdf');
 
 
 const { response } = require('express');
-// const { ObjectId } = require('mongoose').Types;
 
 const { generatePdf, renderTemplate } = require('../helpers/generatePdf');
 
@@ -31,12 +30,14 @@ const allowCollections = [
 
 
 const filter = async( req, res = response ) => {
+    const user = req.user._id;
     const { collection, field, value } = req.params;
 
 
     const searchP = {
         field,
         value,
+        user
     }
 
 
@@ -75,9 +76,9 @@ const filter = async( req, res = response ) => {
 
 const verbsFilter = async( searchParams, res ) => {
 
-    const { field, value } = searchParams;
+    const { field, value, user } = searchParams;
 
-    const verbs = await Verb.find({ status: true }).where( field, value );
+    const verbs = await Verb.find({ status: true, user }).where( field, value );
 
     const data = {
         verbs: JSON.parse(JSON.stringify(verbs))
@@ -133,82 +134,148 @@ const userFilter = async( searchParams, res ) => {
 
 const adjectivesFilter = async( searchParams, res ) => {
 
-    const { field, value } = searchParams;
+    const { field, value, user } = searchParams;
 
-    const adjectives = await Adjective.find({ status: true }).where( field, value );
+    const adjectives = await Adjective.find({ status: true, user, field });
 
     const data = {
         adjectives: JSON.parse(JSON.stringify(adjectives))
     }
 
-    // const html = renderTemplate('./templates/verbsTemplate.html', data );
+    const html = renderTemplate('./templates/adjectivesTemplate.html', data );
 
-    // generatePdf(html);
+    pdf.create(html).toStream((err, pdfStream) => {
+        if (err) {   
+          // handle error and return a error response code
+          console.log(err)
+          return res.status(400).json({
+              msg: 'Pdf error, we could not created it'
+          })
+        } else {
+          // send a status code of 200 OK
+          res.statusCode = 200             
+          res.contentType("application/pdf");
+          // once we are done reading end the response
+          pdfStream.on('end', () => {
+            // done reading
+            return res.end()
+          })
+    
+          // pipe the contents of the PDF directly to the response
+          pdfStream.pipe(res)
+        }
+      })
 
-    return res.json({
-       results: ( adjectives ) ?  adjectives  : []
-   });
+    
 }
 
 
 const phrasalVFilter = async( searchParams, res ) => {
 
-    const { field, value } = searchParams;
+    const { field, user } = searchParams;
 
-    const phrasalV = await PhrasalVerb.find({ status: true }).where( field, value );
+    const phrasalV = await PhrasalVerb.find({ status: true, user, field });
 
     const data = {
         phrasalV: JSON.parse(JSON.stringify(phrasalV))
     }
 
-    // const html = renderTemplate('./templates/verbsTemplate.html', data );
+    const html = renderTemplate('./templates/phrasalVTemplate.html', data );
 
-    // generatePdf(html);
-
-    return res.json({
-       results: ( phrasalV ) ?  phrasalV  : []
-   });
+    pdf.create(html).toStream((err, pdfStream) => {
+        if (err) {   
+          // handle error and return a error response code
+          console.log(err)
+          return res.status(400).json({
+              msg: 'Pdf error, we could not created it'
+          })
+        } else {
+          // send a status code of 200 OK
+          res.statusCode = 200             
+          res.contentType("application/pdf");
+          // once we are done reading end the response
+          pdfStream.on('end', () => {
+            // done reading
+            return res.end()
+          })
+    
+          // pipe the contents of the PDF directly to the response
+          pdfStream.pipe(res)
+        }
+      })
 }
 
 
 
 const prepositionFilter = async( searchParams, res ) => {
 
-    const { field, value } = searchParams;
+    const { field, value, user } = searchParams;
 
-    const prepositions = await Preposition.find({ status: true }).where( field, value );
+    const prepositions = await Preposition.find({ status: true, user, field })
 
     const data = {
         prepositions: JSON.parse(JSON.stringify(prepositions))
     }
 
-    // const html = renderTemplate('./templates/verbsTemplate.html', data );
+    const html = renderTemplate('./templates/prepositionsTemplate.html', data );
 
-    // generatePdf(html);
-
-    return res.json({
-       results: ( prepositions ) ?  prepositions  : []
-   });
+    pdf.create(html).toStream((err, pdfStream) => {
+        if (err) {   
+          // handle error and return a error response code
+          console.log(err)
+          return res.status(400).json({
+              msg: 'Pdf error, we could not created it'
+          })
+        } else {
+          // send a status code of 200 OK
+          res.statusCode = 200             
+          res.contentType("application/pdf");
+          // once we are done reading end the response
+          pdfStream.on('end', () => {
+            // done reading
+            return res.end()
+          })
+    
+          // pipe the contents of the PDF directly to the response
+          pdfStream.pipe(res)
+        }
+      })
 }
 
 
 const connectorsFilter = async( searchParams, res ) => {
 
-    const { field, value } = searchParams;
+    const { field, value, user } = searchParams;
 
-    const connectors = await Connector.find({ status: true }).where( field, value );
+    const connectors = await Connector.find({ status: true, field, user });
 
     const data = {
         connectors: JSON.parse(JSON.stringify(connectors))
     }
 
-    // const html = renderTemplate('./templates/verbsTemplate.html', data );
+    const html = renderTemplate('./templates/connectorsTemplate.html', data );
 
-    // generatePdf(html);
-
-    return res.json({
-       results: ( connectors ) ?  connectors  : []
-   });
+    pdf.create(html).toStream((err, pdfStream) => {
+        if (err) {   
+          // handle error and return a error response code
+          console.log(err)
+          return res.status(400).json({
+              msg: 'Pdf error, we could not created it'
+          })
+        } else {
+          // send a status code of 200 OK
+          res.statusCode = 200             
+          res.contentType("application/pdf");
+          // once we are done reading end the response
+          pdfStream.on('end', () => {
+            // done reading
+            return res.end()
+          })
+    
+          // pipe the contents of the PDF directly to the response
+          pdfStream.pipe(res)
+        }
+      })
 }
 
 
